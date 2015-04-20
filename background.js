92220a315a6bd2f5e8710b58f3c63443adfdd99b
sheryllun <sheryl.v.lun@gmail.com>
2015-04-20 13:08:17 -0400

    Work on loading saved options

--- background.js
+++ background.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
   if(!('Notification' in window)) {
     $('#notification').text('Sorry, your browser does not support the Web Notifications API.');
-  } else console.log('Web Notifications API supported');
+  }
+
+  if(localStorage.saved) {
+    userPreferences.load();
+    checkStatus();
+  } else {
+    userPreferences.init();
+  }
+
 });
 
 
@@ -47,31 +55,80 @@ $(document).ready(function() {
     }
   }
 
-  function savePreferences() {
-    localStorage.setItem('enabled', $('input[name="default"]:checked').val());
-    localStorage.setItem('time', $('input[name="time"]').val());
-    localStorage.setItem('close', $('input[name="close"]:checked').val());
+var userPreferences = {
 
-    if($('#walk').is(':checked')) {
-      localStorage.setItem('walk', 'checked');
-    } else {
-      localStorage.setItem('walk', 'not checked');
+  enableQuestion: $('input[name="default"]'),
+  timeQuestion: $('input[name="time"]'),
+  closeQuestion: $('input[name="close"]'),
+  walkQuestion: $('input[name="walk"]'),
+
+  init: function() {
+    this.enableQuestion.filter('[value="1"]').prop('checked', true);
+    this.timeQuestion.val('6');
+    this.closeQuestion.filter('[value="1"]').attr('checked', 'checked');
+    this.walkQuestion.prop('checked', false);
+  },
+
+  load: function() {
+    var enabledOption = localStorage.enabled;
+    var timeOption = localStorage.time;
+    var closeOption = localStorage.close;
+    var walkOption = localStorage.walk;
+
+    $.each(this.enableQuestion, function() {
+      if($(this).val() == enabledOption) {
+        $(this).attr('checked', 'checked');
+      }
+    });
+
+    this.timeQuestion.val(timeOption);
+
+    $.each(this.closeQuestion, function() {
+      if($(this).val() == closeOption) {
+        $(this).attr('checked', 'checked');
+      }
+    });
+
+    if(walkOption == 'checked') {
+      this.walkQuestion.prop('checked', 'checked');
     }
-    updateStatus();
-    console.log(localStorage.enabled + " " + localStorage.time + " " + localStorage.close + " " + localStorage.walk);
-  }
+  },
+
+  save: function() {
+      localStorage.setItem('enabled', $('input[name="default"]:checked').val());
+      localStorage.setItem('time', $('input[name="time"]').val());
+      localStorage.setItem('close', $('input[name="close"]:checked').val());
+
+      if($('#walk').is(':checked')) {
+        localStorage.setItem('walk', 'checked');
+      } else {
+        localStorage.setItem('walk', 'not checked');
+      }
+      
+      updateStatus();
+      setTimeout(function() {
+        checkStatus();
+      }, 1000);
+
+      console.log(localStorage.enabled + " " + localStorage.time + " " + localStorage.close + " " + localStorage.walk);
+    }
+};
 
   function updateStatus() {
-    var currentStatus = localStorage.enabled;
     $('#notification').html('Options saved.');
-    setTimeout(function() {
-      if(currentStatus == 0) {
-        $('#notification').html('Reminders are currently <strong>disabled</strong>.');
-      } else {
-        $('#notification').html('Reminders are currently <strong>enabled</strong>.');
-      }
-    }, 1000);
+
   }
+
+  function checkStatus() {
+    var currentStatus = localStorage.enabled;
+    if(currentStatus == 0) {
+      $('#notification').html('Reminders are currently <strong>disabled</strong>.');
+    } else {
+      $('#notification').html('Reminders are currently <strong>enabled</strong>.');
+    }
+  }
+
+
 
 
   $('#message').click(function() {
@@ -80,7 +137,8 @@ $(document).ready(function() {
 
   $('#submit').click(function(e) {
     e.preventDefault();
-    savePreferences();
+    localStorage.setItem('saved', 'true');
+    userPreferences.save();
   });
 
 
