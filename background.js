1c149d71cbd4934e9948a3152284828522cde22b
sheryllun <sheryl.v.lun@gmail.com>
2015-04-24 15:36:24 -0400

    Add options, try to connect saved options with app

--- background.js
+++ background.js
@@ -1,10 +1,22 @@
 $(document).ready(function() {
-  if(!('Notification' in window)) {
-    $('#notification').text('Sorry, your browser does not support the Web Notifications API.');
+  //if Chrome Notifications not supported, try the Web Notifications API.
+  if(!chrome.notifications) {
+    if(!('Notification' in window)) {
+      $('#notification').text('Sorry, your browser does not support the Web Notifications API.');
+    } 
+    else {
+      Notification.requestPermission(function(result) {
+        if(result === 'default') {
+          $('#notification').html('Desktop notifications must be allowed in order for this extension to run. <span id="request">Allow notifications</span>');
+        } else if (result === 'denied') {
+          $('#notification').html('Desktop notifications must be allowed in order for this extension to run. Please remove notification blocking from Chrome Settings > Advanced Settings > Privacy > Content Settings > ');
+        }
+      });
+    }
   }
 
   if(localStorage.saved) {
-    userPreferences.load();
+    userPreferences.loadDom();
     checkStatus();
   } else {
     userPreferences.init();
@@ -12,127 +24,87 @@ $(document).ready(function() {
 
 });
 
+  // $('#notification').on('click', 'span', function() {
+  //   Notification.requestPermission();
+  // });
 
-  function renderMessage() {
-    var postureBeginning =['Straighten up, ', 'Shoulders back, ', 'How\'s your posture, ', 'Beep, posture please, ', 'Check your posture, ', 'Sit up straight, ', 'Check yourself, ', 'No hunchbacks, ', 'At attention, ', 'Stop slumping, ', 'Mother always told you not to slouch, ', 'Sit up, ', 'Posture Reminder, ', 'Posture police, '],
-			 postureEnd = ['young grasshopper.', 'buddy.', 'amigo.', 'Quasimodo.', 'bruh.', 'boss.'],
-			 begInt = Math.floor(Math.random() * postureBeginning.length),
-			 endInt = Math.floor(Math.random() * postureEnd.length),
-			 fullMessage = postureBeginning[begInt] + postureEnd[endInt];
-        return fullMessage;
-    }
-
-  function displayMessage() {
-    var title = 'Your PostureMinder';
-    var messageBody = renderMessage();
-
-    if(Notification.permission === "granted") {
-      var notification = new Notification(title, {
-        body: messageBody,
-        icon: 'img/spine.png'
-      });
-      setTimeout(function(){
-        notification.close();
-      },5000);
-
-    } else if (Notification.permission !== 'denied') {
-      Notification.requestPermission(function(permission){
-
-        if(permission === 'granted')  {
-          var notification = new Notification(title, {
-                  body: messageBody,
-                  icon: 'img/spine.png'
-                });
-                setTimeout(function(){
-                  notification.close();
-                },5000);
-
-          } else {
-            $('#notification').text('Desktop notifications must be allowed in order for this extension to run.');
-            return;
-          }
-      });
-    }
-  }
-
-var userPreferences = {
-
-  enableQuestion: $('input[name="default"]'),
-  timeQuestion: $('input[name="time"]'),
-  closeQuestion: $('input[name="close"]'),
-  walkQuestion: $('input[name="walk"]'),
+var reminder = {
 
   init: function() {
-    this.enableQuestion.filter('[value="1"]').prop('checked', true);
-    this.timeQuestion.val('6');
-    this.closeQuestion.filter('[value="1"]').attr('checked', 'checked');
-    this.walkQuestion.prop('checked', false);
-  },
-
-  load: function() {
-    var enabledOption = localStorage.enabled;
-    var timeOption = localStorage.time;
-    var closeOption = localStorage.close;
-    var walkOption = localStorage.walk;
-
-    $.each(this.enableQuestion, function() {
-      if($(this).val() == enabledOption) {
-        $(this).attr('checked', 'checked');
-      }
-    });
-
-    this.timeQuestion.val(timeOption);
-
-    $.each(this.closeQuestion, function() {
-      if($(this).val() == closeOption) {
-        $(this).attr('checked', 'checked');
-      }
-    });
-
-    if(walkOption == 'checked') {
-      this.walkQuestion.prop('checked', 'checked');
-    }
-  },
-
-  save: function() {
-      localStorage.setItem('enabled', $('input[name="default"]:checked').val());
-      localStorage.setItem('time', $('input[name="time"]').val());
-      localStorage.setItem('close', $('input[name="close"]:checked').val());
-
-      if($('#walk').is(':checked')) {
-        localStorage.setItem('walk', 'checked');
-      } else {
-        localStorage.setItem('walk', 'not checked');
-      }
-      
-      updateStatus();
-      setTimeout(function() {
-        checkStatus();
-      }, 1000);
-
-      console.log(localStorage.enabled + " " + localStorage.time + " " + localStorage.close + " " + localStorage.walk);
-    }
-};
-
-  function updateStatus() {
-    $('#notification').html('Options saved.');
-
-  }
-
-  function checkStatus() {
-    var currentStatus = localStorage.enabled;
-    if(currentStatus == 0) {
-      $('#notification').html('Reminders are currently <strong>disabled</strong>.');
+    var prefs = userPreferences.getPreferences();
+    var time = prefs.timeOption * 10000;
+    if(prefs.enabledOption == 0) {
+      return;
     } else {
-      $('#notification').html('Reminders are currently <strong>enabled</strong>.');
+      this.displayMessage(time);
     }
+  },
+
+  closeReminder: function() {
+
+  },
+
+  renderMessage: function() {
+    var postureBeginning =['Straighten up, ', 'Shoulders back, ', 'How\'s your posture, ', 'Beep, posture please, ', 'Check your posture, ', 'Sit up straight, ', 'Check yourself, ', 'No hunchbacks, ', 'At attention, ', 'Stop slumping, ', 'Mother always told you not to slouch, ', 'Sit up, ', 'Posture Reminder, ', 'Posture police, '],
+       postureEnd = ['young grasshopper.', 'buddy.', 'amigo.', 'Quasimodo.', 'boss.'],
+       begInt = Math.floor(Math.random() * postureBeginning.length),
+       endInt = Math.floor(Math.random() * postureEnd.length),
+       fullMessage = postureBeginning[begInt] + postureEnd[endInt];
+        return fullMessage;
+    },
+
+  displayMessage: function(time) {
+    var timer = setInterval(function() {
+      var title = 'Your PostureMinder';
+      var messageBody = reminder.renderMessage();
+      var options = {
+        type: 'basic',
+        title: 'Your PostureMinder',
+        message: messageBody,
+        iconUrl: 'img/spine.png'
+      };
+
+      if(chrome.notifications) {
+          chrome.notifications.clear('PostureMinder');
+          chrome.notifications.create('PostureMinder', options);
+      }
+      // } else if(Notification.permission === "granted") {
+      //     var notification = new Notification(title, {
+      //       body: messageBody,
+      //       icon: 'img/spine.png'
+      //     });
+      //     setTimeout(function(){
+      //       notification.close();
+      //     },5000);
+
+      //   } else if (Notification.permission !== 'denied') {
+      //     Notification.requestPermission(function(permission){
+
+      //       if(permission === 'granted')  {
+      //         var notification = new Notification(title, {
+      //                 body: messageBody,
+      //                 icon: 'img/spine.png'
+      //               });
+      //               setTimeout(function(){
+      //                 notification.close();
+      //               },5000);
+
+      //         } else {
+      //           $('#notification').text('Desktop notifications must be allowed in order for this extension to run.');
+      //           return;
+      //         }
+      //    });
+      //  }
+    }, time);
+    
   }
 
+};
 
 
 
   $('#message').click(function() {
-    displayMessage();
+    reminder.displayMessage();
   });
 
   $('#submit').click(function(e) {
@@ -140,5 +112,7 @@ var userPreferences = {
     localStorage.setItem('saved', 'true');
     userPreferences.save();
   });
+
+reminder.init();
 
 
---
+++ options.js
@@ -0,0 +1,79 @@
+var userPreferences = {
+
+  enableQuestion: $('input[name="default"]'),
+  timeQuestion: $('input[name="time"]'),
+  closeQuestion: $('input[name="close"]'),
+  walkQuestion: $('input[name="walk"]'),
+
+  init: function() {
+    this.enableQuestion.filter('[value="1"]').prop('checked', true);
+    this.timeQuestion.val('6');
+    this.closeQuestion.filter('[value="1"]').attr('checked', 'checked');
+    this.walkQuestion.prop('checked', false);
+  },
+
+  getPreferences: function() {
+    var allPrefs = {
+      enabledOption: localStorage.enabled,
+      timeOption: localStorage.time,
+      closeOption: localStorage.close,
+      walkOption :localStorage.walk
+    };
+
+    return allPrefs;
+  },
+
+  loadDom: function() {
+    var preferences = this.getPreferences();
+
+    $.each(this.enableQuestion, function() {
+      if($(this).val() == preferences.enabledOption) {
+        $(this).attr('checked', 'checked');
+      }
+    });
+
+    this.timeQuestion.val(preferences.timeOption);
+
+    $.each(this.closeQuestion, function() {
+      if($(this).val() == preferences.closeOption) {
+        $(this).attr('checked', 'checked');
+      }
+    });
+
+    if(preferences.walkOption == 'checked') {
+      this.walkQuestion.prop('checked', 'checked');
+    }
+  },
+
+  save: function() {
+      localStorage.setItem('enabled', $('input[name="default"]:checked').val());
+      localStorage.setItem('time', $('input[name="time"]').val());
+      localStorage.setItem('close', $('input[name="close"]:checked').val());
+
+      if($('#walk').is(':checked')) {
+        localStorage.setItem('walk', 'checked');
+      } else {
+        localStorage.setItem('walk', 'not checked');
+      }
+      
+      updateStatus();
+      setTimeout(function() {
+        checkStatus();
+      }, 1000);
+
+      console.log(localStorage.enabled + " " + localStorage.time + " " + localStorage.close + " " + localStorage.walk);
+    }
+};
+
+  function updateStatus() {
+    $('#notification').html('Options saved.');
+  }
+
+  function checkStatus() {
+    var currentStatus = localStorage.enabled;
+    if(currentStatus == 0) {
+      $('#notification').html('Reminders are currently <strong>disabled</strong>.');
+    } else {
+      $('#notification').html('Reminders are currently <strong>enabled</strong>.');
+    }
+  }
